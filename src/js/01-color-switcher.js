c49306dd1be82c6b998079dc478fd117e1e34c78
KosminaOlena <ok.art2017@gmail.com>
2023-03-19 12:26:36 +0200

    update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -16,11 +16,9 @@ function onStartBtn() {
     
 }
 function onStopBtn() {
-    if (startBtn.hasAttribute("disabled")) {
         clearInterval(timerId);
         startBtn.disabled = false;
         stopBtn.disabled = true;
-    }
 }
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
